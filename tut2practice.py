d227530e390132eb3690b64f6bc3c96242641426
William <wdnconsultancy@yahoo.com>
2023-06-01 19:29:30 +0800

    Tut2practice OOP python

---
+++ tut2practice.py
@@ -0,0 +1,34 @@
+# create a class with class and instance variables
+# utilise dict method to see 
+
+class Person:
+
+    grow_up = 2
+    name_sample = "Spiderman"
+
+    def __init__(self, age, name, occupation):
+        self.age = age
+        self.name = name
+        self.occupation = occupation 
+    
+    # by putting self can allow overwrite of class variable 
+    def grown(self):
+        # self.age += Person.grow_up   , in this case cant overwrite
+        self.age += self.grow_up
+
+bill = Person(21, "BILL", "Quant")
+
+print(bill.age)
+print(bill.__dict__)
+
+bill.grown()
+print(bill.__dict__)
+
+# here we overwrite by creating within the instance a variable with the same name as the class variable
+#  python will look at the instance for highest piority than the class after if that variabel not in inside which is why this works
+bill.grow_up = 90
+print(bill.__dict__)
+
+bill.grown()
+print(bill.__dict__)
+

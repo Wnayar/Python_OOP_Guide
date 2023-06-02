57f79d0c391292d243bac74215a8683796f945ff
William <wdnconsultancy@yahoo.com>
2023-06-02 23:44:12 +0800

    Tut6 skipped for now will come back in the future

---
+++ tut6.py
@@ -0,0 +1,4 @@
+# Python OOP Tutorial 6: Python OOP Tutorial 6: Property Decorators - Getters, Setters, and Deleters
+# https://youtu.be/jCzT9XFZ5bw
+
+# Skipped part 6 of the series as he was discussing Property decorators: getters, setters and deleters,  which did not seem relevant at the time and was fairly niche, but will come back in the future to code this
---
+++ tut6practice.py
@@ -0,0 +1,1 @@
+# Skipped part 6 of the series as he was discussing Property decorators: getters, setters and deleters,  which did not seem relevant at the time and was fairly niche, but will come back in the future to code this

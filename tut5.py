5cf7cc0f1c0a00a96fdc2e7717ebcc86552618b9
William <wdnconsultancy@yahoo.com>
2023-06-02 23:38:33 +0800

    TUT5 SKIPPED for now Come back in the future

---
+++ tut5.py
@@ -0,0 +1,4 @@
+# Python OOP Tutorial 5: Special (Magic/Dunder) Methods
+# https://youtu.be/3ohzBxoFHAY
+
+# Skipped part 5 of the series as he was discussing __repr__ and __str__ which did not seem relevant at the time and was fairly niche, but will come back in the future to code this
---
+++ tut5practice.py
@@ -0,0 +1,1 @@
+# Skipped part 5 of the series as he was discussing __repr__ and __str__ which did not seem relevant at the time and was fairly niche, but will come back in the future to code this

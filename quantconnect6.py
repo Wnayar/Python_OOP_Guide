9708b51bf88dfde6ec14fade9e0386b97c2ba1d2
William <wdnconsultancy@yahoo.com>
2023-06-14 16:39:12 +0800

    quantconnect6 more information on super()__init__

--- quantconnect6.py
+++ quantconnect6.py
@@ -10,6 +10,8 @@ class Myalgo_weirdname(QCAlgorithm):
     def intialize(self):
         self.returns = 10
         self.universe =  ['BTCUSD', 'LTCUSD', 'ETHUSD']
+        # passing in self which is this current instance of your algorithm to the  class in this case. In quant connect sometimes they use helper functions instead and dont show the passing of self in
+        # in cases where done need the reference to the main algorithm can pass int he QCAlgorithm base class 
         self.pairs = Pair(self, self.universe[0])
 
 class Pair:
@@ -26,3 +28,37 @@ print(will.__dict__)
 print(will.pairs.ticker)
 
 
+# testing super init constructor, if miss out constructor of parent in the super will get thrown an eror
+#  so what happens here is the user inputs in the BillAlgorithm class, get passed on those variables to the super(parent) class
+#  so in this case when the user types in APPL, and 10 those variables are passe sinto the parent class init function to intialise it, then the price variable we handle it as a modifciatin
+class WillAlgorithm:
+    def __init__(self, stock, returns):
+        self.stock = stock
+        self.returns = returns
+
+class BillAlgorithm(WillAlgorithm):
+    def __init__(self, stock, returns, price):
+        super().__init__(stock, returns)
+        self.price = price
+
+chungus = BillAlgorithm("APPL", 10, 999)
+
+print(chungus.__dict__)
+
+
+# so below here we are passing in the values True and None to the parent class WillAlgorithm2, which stores it under stock and returns
+# so super() is just esnetially passign the variables u specifify to the parent init function which will then intialize whatever is there 
+# this testign came about due to lesson 10 task 5 of the equity bootcamp quant connect https://www.quantconnect.com/learning/task/174/Using-Fundamental-Data-to-Emit-Insights
+# where they passed in true and none into the super, which now I know is just passing variabel to the parent init method
+class WillAlgorithm2:
+    def __init__(self, stock, returns):
+        self.stock = stock
+        self.returns = returns
+
+class BillAlgorithm2(WillAlgorithm2):
+    def __init__(self):
+        super().__init__(True, None)
+
+bigchungus = BillAlgorithm2()
+
+print(bigchungus.__dict__)

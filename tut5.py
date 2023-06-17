89e297f775749dde3d5702c573e4d715b4d8432d
William <wdnconsultancy@yahoo.com>
2023-06-17 11:29:17 +0800

    OOP tut 5 and practice, dunder methods

--- tut5.py
+++ tut5.py
@@ -1,4 +1,82 @@
 # Python OOP Tutorial 5: Special (Magic/Dunder) Methods
 # https://youtu.be/3ohzBxoFHAY
 
-# Skipped part 5 of the series as he was discussing __repr__ and __str__ which did not seem relevant at the time and was fairly niche, but will come back in the future to code this
+
+# DUNDER/MAGIC METHODS, __repr__, __str__, etc
+
+
+class Employee:
+
+    raise_amt = 1.04
+    
+    # dunder/magic numbers are the double underscore method __init__ etc, __repr__, __str__
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+    
+    # __repr__ is more so for developers, __str__ is more so for user
+    # if there is not __str__, when call __str__ it will fall on __repr__
+    # good practice is code that cna recreate the object
+    # so it changes the return from being the object location at hexidiecimal etc into this format instead
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    # if you have the str method the print will default to this one, but u can still call those repr or str functions
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+    
+    # left side of addtion is self, right side is the other
+    def __add__(self, other):
+        return self.pay + other.pay
+    
+    # just showcasing anogther dunder method __len__
+    def __len__(self):
+        return len(self.fullname())
+    
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+print(emp_1)
+# showcasing different ways to call the dunder/magiv methods
+print(emp_1.__repr__())
+print(repr(emp_1))
+print(emp_1.__str__())
+print(str(emp_1))
+
+# there are many dunder methods
+print(1 + 2)
+print(int.__add__(1, 2))
+print(str.__add__('a', 'b'))
+
+# __add__ method in class
+print(emp_1 + emp_2)
+
+# there are many dunder methods for arthmethics just refer to the documentation 
+print(len("test"))
+# so above is actually just calling a dunder method, because below does the same thing
+# so if you think about it the "test", is an instance of the string class and we are callign the inbuilt dunder __len__ method on it 
+# that is why if u call len on my employee class it will get an error
+print('test'.__len__())
+
+# so if you wanted the len dunder method to work on our object we have to created the len dunder method
+print(len(emp_1))
+
+# in documentation you may see if, then later return not implemented, what tshi means if this object doesnt know how to handle it, it wont throw an error it will see if the other object knows how to do it
+
+# also side note string literal vs string object 
+# https://stackoverflow.com/questions/61982661/how-can-a-string-object-be-built-from-a-string-literal see answer by DanielB
+# x is assigned an integer valye with the literal "5"
+x = 5 
+print(isinstance(x, int))
+# foo is assigned a a str value using the literal "hello"
+foo = "hello"
+print(isinstance(foo, str))
--- tut5practice.py
+++ tut5practice.py
@@ -1,1 +1,43 @@
-# Skipped part 5 of the series as he was discussing __repr__ and __str__ which did not seem relevant at the time and was fairly niche, but will come back in the future to code this
+# create a class with __repr__, __str__, and another dunder method if your choice
+# test the functionality of these dunder method and call them in the 2 different ways which produce the same outcome etc, str() or instance.__str__()
+ 
+class Human:
+
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+    
+    # good practice to format it such that it can be replicated in code when copy and past to make the object
+    # repr is more so for developers so that why the code is made to be abel to copy and paste as best practice
+    def __repr__(self):
+        return "Human({}, '{}')".format(self.age, self.name)
+    
+    # __str__ is made mroe so for the user interface, so you can put what format you want the user to see slight diff from above which is wantign it to be able to reproduce the code as this is diff target audience
+    def __str__(self):
+        return "You called the instance of the Human object with the name {}".format(self.name)
+    
+    # __add__ dunded/magic method
+    def __add__(self, other):
+        return self.name + ' ' + other.name
+
+
+bill = Human(21, "Bill")
+will = Human(20, "Will")
+print(bill)
+print(repr(bill))
+# when use print, it will deafult to str if have the method, if not will default to repr, can still call these methods individually 
+print(str(bill))
+
+# here below is just different ways to call the same dunder methods
+print(repr(bill))
+print(bill.__repr__())
+
+print(str(bill))
+print(bill.__str__())
+# and pritn auto defaults to __str__ method if have it, instead of object at 0x... hexidecimal takes away anonymity for end user
+print(bill)
+
+# exploring other dunder methods there are many such as __add__
+print(1 + 2)
+print(int.__add__(1, 2))
+print(will + bill)

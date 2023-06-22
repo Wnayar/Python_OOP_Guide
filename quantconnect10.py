e90f003334854c8b113a46b72a7db60b1e530f57
William <wdnconsultancy@yahoo.com>
2023-06-22 19:50:25 +0800

    quantconnect10 still working on it

--- quantconnect10.py
+++ quantconnect10.py
@@ -1,3 +1,5 @@
+# SECTION A
+
 # testing lists additions
 templist = [1, 2, 3]
 templist1 = [True, False]
@@ -11,6 +13,8 @@ for x in templist + templist1 + templist2:
     print(x)
 
 
+
+# SECTION B
 
 # some random testing of super(), nothing realy learnt below was just testing
 #  intersting points default arguments placed after non default
@@ -35,7 +39,7 @@ print(billy2.__dict__)
 
  
 
-
+# SECTION C
 
 # just a quick refersher on OOP, cant just randomly write self.age = 21
 class test:
@@ -47,8 +51,43 @@ class test:
     def age(self, number):
         self.age = number
     
-   
-
 baz = test()
 baz.age(21)
 print(baz.__dict__)
+
+
+
+
+
+# SECTION D
+class Omega:
+    def OnSecuritiesChanged(self, changes):
+        newchanges = [200 + x for x in changes]
+        for x in newchanges:
+            print(x)
+
+
+class Alpha(Omega):
+    def OnSecuritiesChanged(self, changes):
+        newchanges = [100 + x for x in changes]
+        for x in newchanges:
+            print(x)
+
+
+
+class Beta(Alpha):
+    def OnSecuritiesChanged(self, changes):
+        for x in changes:
+            print(x)
+        
+        # the reason quantconnect is set up as such callign the super(), is it allow su to modify want to uwant, then u just have to call back the parent function to do whatever else it was meant to do you can read the documentation
+        # https://www.quantconnect.com/docs/v2/writing-algorithms/algorithm-framework/portfolio-construction/key-concepts
+        #  and on the lean engine source code line 150 onsecuriteschanged https://www.lean.io/docs/v2/lean-engine/class-reference/PortfolioConstructionModel_8cs_source.html
+        # super seems to check the parent class, if its not there it will keep checking one level up in the method resolution order
+        #  this can be proved by commenting out the function in Alpha and it will call from Omega 
+        super().OnSecuritiesChanged(changes)
+    
+    
+
+chungus = Beta()
+chungus.OnSecuritiesChanged([1, 2, 3, 4])
